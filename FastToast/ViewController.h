221f139bb36b10ae1c3dcfd80feb03104312a887
allen <379306634@qq.com>
2018-04-27 20:21:03 +0800

    Initial Commit

---
+++ FastToast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastToast
+//
+//  Created by lengshengren on 2018/4/27.
+//  Copyright © 2018年 Lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastToast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastToast
+//
+//  Created by lengshengren on 2018/4/27.
+//  Copyright © 2018年 Lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
